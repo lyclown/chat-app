a6e5fc925962aab4d87d0fbadc61706f304fa46f
李耀 <237057917@qq.com>
2023-05-06 18:35:01 +0800

    设置yml文件

--- src/router/index.js
+++ src/router/index.js
@@ -3,36 +3,33 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
+const routes = [{
     path: '/',
     name: 'chat',
-    component: ()=>import('@/components/layout/BasicsLayout'),
-    redirect:'/chat',
-    children:[
-      {
-        path: '/chat',
-        name: 'chat',
-        component: ()=>import('@/views/chat/ChatGPT.vue'),
-      },
-      {
-        path: '/image',
-        name: 'image',
-        component: ()=>import('@/views/chat/ChatImage.vue'),
-      },
-      {
-        path: '/video',
-        name: 'video',
-        component: ()=>import('@/views/chat/ChatVideo.vue'),
-      }
+    component: () => import('@/components/layout/BasicsLayout'),
+    redirect: '/chat',
+    children: [{
+            path: '/chat',
+            name: 'chat',
+            component: () => import('@/views/chat/ChatGPT.vue'),
+        },
+        {
+            path: '/image',
+            name: 'image',
+            component: () => import('@/views/chat/ChatImage.vue'),
+        },
+        {
+            path: '/video',
+            name: 'video',
+            component: () => import('@/views/chat/ChatVideo.vue'),
+        }
     ]
 
-  },
-]
+}, ]
 
 const router = new VueRouter({
-  mode: 'hash',
-  routes
+    mode: 'hash',
+    routes
 })
 
 export default router
